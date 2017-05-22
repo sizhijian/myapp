8a9298a3433ee320381af4fedbf33b6438acb3c4
sizhijian619 <sizhijian619@163.com>
2017-05-22 11:00:01 +0800

    delete some files

--- express_demo.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-app = express();
-app.get('/', function(req, res) {
-	res.send("<h1>title</h1>")
-})
-app.listen(3000, function() {
-	console.log("The service is listening on http://localhost:3000")
-});
